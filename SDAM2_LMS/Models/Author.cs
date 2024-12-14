0254a048f9a0c261f95762c76d226c38edcbe3af
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2024-12-14 12:35:21 +0530

    feat: Book functionality initial

---
+++ SDAM2_LMS/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal class Author
+    {
+        public string AuthorName { get; protected set; }
+        public string Contact { get; protected set; }
+
+    }
+}
