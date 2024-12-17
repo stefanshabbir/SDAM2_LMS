701ba4f0d00f8f413bf2847760856347349b39ee
stefanshabbir <stefanshabbir@gmail.com>
2024-12-17 16:20:26 +0530

    feat: Add Account

---
+++ SDAM2_LMS/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal class Account
+    {
+        private string Username { get; set; }
+        private string Password { get; set; }
+        private int AccountID { get; set; }
+
+
+        public Account(string username, string password, int accountId)
+        {
+            this.Username = username;
+            this.Password = password;   
+            this.AccountID = accountId;
+        }
+
+        public void ResetPassword(string newPassword)
+        {
+            Password = newPassword;
+        }
+    }
+}
