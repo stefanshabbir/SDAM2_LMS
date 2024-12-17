edbda65b1e3018df0cb6725f70e77cc9d62b20fa
stefanshabbir <stefanshabbir@gmail.com>
2024-12-17 16:40:07 +0530

    feat: Add Member

---
+++ SDAM2_LMS/Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal class Member : Account
+    {
+        private DateTime DateOfMembership { get; set; }
+        private List<Book> BooksCheckedOut { get; set; }
+
+        public Member(string username, string password, int accountId, DateTime dateOfMembership,List<Book> booksCheckedOut) : base(username, password, accountId)
+        {
+
+        }
+
+        public void returnBook(Book book)
+        {
+            BooksCheckedOut.Remove(book);
+        }
+    }
+}
