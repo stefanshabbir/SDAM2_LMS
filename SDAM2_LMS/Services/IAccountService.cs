58e832a41fdc9dbed98d0f23685fe0378adaab4c
stefanshabbir <stefanshabbir@gmail.com>
2024-12-18 00:56:32 +0530

    feat: add Service Layer for Business Logic

---
+++ SDAM2_LMS/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal interface IAccountService
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password, string confirmPassword);
+    }
+}
