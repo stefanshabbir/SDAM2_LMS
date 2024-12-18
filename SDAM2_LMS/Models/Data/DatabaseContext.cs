aa636cb611687ed64892a42df0d6af4dbf19d7a6
Yumna-H <yumnahassan06@gmail.com>
2024-12-18 10:16:53 +0530

    Deletion of Database
    Deleted a duplicate database

--- SDAM2_LMS/Models/Data/DatabaseContext.cs
+++ SDAM2_LMS/Models/Data/DatabaseContext.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Entit
 
 namespace SDAM2_LMS.Models.Data
 {
     internal class DatabaseContext
     {
-        protected override void OnConfiguring() 
+
     }
 }
