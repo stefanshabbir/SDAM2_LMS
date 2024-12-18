d61023f43efe157781c0b42092e20d0bf6743bde
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2024-12-18 09:16:11 +0530

    feat: Addes PersonalID class

--- SDAM2_LMS/Models/Account.cs
+++ SDAM2_LMS/Models/Account.cs
@@ -8,16 +8,18 @@ namespace SDAM2_LMS.Models
 {
     internal class Account : IAccount
     {
+        public int AccountID { get; private set; }
+        public int PersonalInfo { get; private set; }
         public string Username { get; private set; }
         public string Password { get; private set; }
-        public int AccountID { get; private set; }
 
 
-        public Account(string username, string password, int accountId)
+        public Account(string username, string password, int accountId, int personalInfoID)
         {
             this.Username = username;
-            this.Password = password;   
+            this.Password = password;
             this.AccountID = accountId;
+            this.PersonalInfo = personalInfoID;
         }
 
         public void ResetPassword(string newPassword)
---
+++ SDAM2_LMS/Models/PersonalID_Info.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal class PersonalID_Info
+    {
+        public int PersonalID { get; private set; }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string PhoneNumber {  get; private set; }
+        public string Address { get; private set; }
+
+        PersonalID_Info(int personalID, string name, string email, string phoneNumber, string address)
+        {
+            PersonalID = personalID;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+    }
+}
