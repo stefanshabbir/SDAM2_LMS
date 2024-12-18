736456711b0d4a5da8a3fd6c0c970fc0d0918b3b
stefanshabbir <stefanshabbir@gmail.com>
2024-12-18 09:37:35 +0530

    feat: Add DatabaseContext Class & Data Layer

---
+++ SDAM2_LMS/Models/Data/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Entit
+
+namespace SDAM2_LMS.Models.Data
+{
+    internal class DatabaseContext
+    {
+        protected override void OnConfiguring() 
+    }
+}
