c7870adabaf8ff66953a15674c18dda2342c72c8
Yumna-H <yumnahassan06@gmail.com>
2024-12-27 00:24:02 +0530

    Update account type in database and add AccountType and Borrowing models

---
+++ SDAM2_LMS/Models/AccountType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal class AccountType
+    {
+        public int AccountTypeID { get; private set; }
+        public string AccountTypeName { get; set; }
+
+        public AccountType(int accountTypeID, string accountTypeName ) 
+        {
+            this.AccountTypeID = accountTypeID;
+            this.AccountTypeName = accountTypeName;
+        }
+    }
+}
---
+++ SDAM2_LMS/Models/Borrowing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Models
+{
+    internal class Borrowing
+    {
+        public int BorrowID { get; private set; }
+        public int BookID { get; private set; }
+        public int AccountID { get; private set; }
+        public DateTime BorrowDate { get; private set; }
+        public DateTime ReturnDate { get; private set; }
+
+        //public Book Book { get; set; }
+        //public Account Account { get; set; }
+
+        public Borrowing (int borrowID, int bookID, int accountID, DateTime borrowDate, DateTime returnDate, Book book, Account account)
+        {
+            this.BorrowID = borrowID;
+            this.BookID = bookID;
+            this.AccountID = accountID;
+            this.BorrowDate = borrowDate;
+            this.ReturnDate = returnDate;
+        }
+    }
+}
