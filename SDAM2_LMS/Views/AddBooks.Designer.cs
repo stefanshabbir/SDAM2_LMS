6757e2adc3673273f2ae9137c62878632579e5f0
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2025-01-20 16:18:29 +0530

    feat: Label renaming and buttons

--- SDAM2_LMS/Views/AddBooks.Designer.cs
+++ SDAM2_LMS/Views/AddBooks.Designer.cs
@@ -31,20 +31,22 @@
             panel1 = new Panel();
             label1 = new Label();
             panel2 = new Panel();
-            button2 = new Button();
-            button1 = new Button();
-            dateTimePicker1 = new DateTimePicker();
-            textBox6 = new TextBox();
-            textBox5 = new TextBox();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
             textBox1 = new TextBox();
-            label7 = new Label();
-            label6 = new Label();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
+            lbl_language = new Label();
+            inpt_genre = new TextBox();
+            lbl_genre = new Label();
+            btn_cancel = new Button();
+            btn_save = new Button();
+            inpt_quantity = new TextBox();
+            inpt_isbn = new TextBox();
+            inpt_publisher = new TextBox();
+            inpt_author = new TextBox();
+            inpt_bookTitle = new TextBox();
+            lbl_quantity = new Label();
+            lbl_isbn = new Label();
+            lbl_publisher = new Label();
+            lbl_author = new Label();
+            lbl_bookTitle = new Label();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             SuspendLayout();
@@ -75,161 +77,183 @@
             // panel2
             // 
             panel2.BackColor = Color.Gainsboro;
-            panel2.Controls.Add(button2);
-            panel2.Controls.Add(button1);
-            panel2.Controls.Add(dateTimePicker1);
-            panel2.Controls.Add(textBox6);
-            panel2.Controls.Add(textBox5);
-            panel2.Controls.Add(textBox3);
-            panel2.Controls.Add(textBox2);
             panel2.Controls.Add(textBox1);
-            panel2.Controls.Add(label7);
-            panel2.Controls.Add(label6);
-            panel2.Controls.Add(label5);
-            panel2.Controls.Add(label4);
-            panel2.Controls.Add(label3);
-            panel2.Controls.Add(label2);
+            panel2.Controls.Add(lbl_language);
+            panel2.Controls.Add(inpt_genre);
+            panel2.Controls.Add(lbl_genre);
+            panel2.Controls.Add(btn_cancel);
+            panel2.Controls.Add(btn_save);
+            panel2.Controls.Add(inpt_quantity);
+            panel2.Controls.Add(inpt_isbn);
+            panel2.Controls.Add(inpt_publisher);
+            panel2.Controls.Add(inpt_author);
+            panel2.Controls.Add(inpt_bookTitle);
+            panel2.Controls.Add(lbl_quantity);
+            panel2.Controls.Add(lbl_isbn);
+            panel2.Controls.Add(lbl_publisher);
+            panel2.Controls.Add(lbl_author);
+            panel2.Controls.Add(lbl_bookTitle);
             panel2.Location = new Point(47, 71);
             panel2.Margin = new Padding(2, 1, 2, 1);
             panel2.Name = "panel2";
-            panel2.Size = new Size(447, 339);
+            panel2.Size = new Size(447, 406);
             panel2.TabIndex = 2;
-            // 
-            // button2
-            // 
-            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(325, 303);
-            button2.Margin = new Padding(2, 1, 2, 1);
-            button2.Name = "button2";
-            button2.Size = new Size(90, 26);
-            button2.TabIndex = 14;
-            button2.Text = "Cancel";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(182, 303);
-            button1.Margin = new Padding(2, 1, 2, 1);
-            button1.Name = "button1";
-            button1.Size = new Size(101, 26);
-            button1.TabIndex = 13;
-            button1.Text = "Save";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(182, 165);
-            dateTimePicker1.Margin = new Padding(2, 1, 2, 1);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(234, 23);
-            dateTimePicker1.TabIndex = 12;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(182, 260);
-            textBox6.Margin = new Padding(2, 1, 2, 1);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(234, 23);
-            textBox6.TabIndex = 11;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(182, 215);
-            textBox5.Margin = new Padding(2, 1, 2, 1);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(234, 23);
-            textBox5.TabIndex = 10;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(182, 121);
-            textBox3.Margin = new Padding(2, 1, 2, 1);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(234, 23);
-            textBox3.TabIndex = 8;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(182, 72);
-            textBox2.Margin = new Padding(2, 1, 2, 1);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(234, 23);
-            textBox2.TabIndex = 7;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(182, 26);
+            textBox1.Location = new Point(182, 211);
             textBox1.Margin = new Padding(2, 1, 2, 1);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(234, 23);
-            textBox1.TabIndex = 6;
+            textBox1.TabIndex = 18;
             // 
-            // label7
+            // lbl_language
             // 
-            label7.AutoSize = true;
-            label7.Location = new Point(46, 262);
-            label7.Margin = new Padding(2, 0, 2, 0);
-            label7.Name = "label7";
-            label7.Size = new Size(53, 15);
-            label7.TabIndex = 5;
-            label7.Text = "Quantity";
+            lbl_language.AutoSize = true;
+            lbl_language.Location = new Point(46, 211);
+            lbl_language.Margin = new Padding(2, 0, 2, 0);
+            lbl_language.Name = "lbl_language";
+            lbl_language.Size = new Size(59, 15);
+            lbl_language.TabIndex = 17;
+            lbl_language.Text = "Language";
             // 
-            // label6
+            // inpt_genre
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(46, 215);
-            label6.Margin = new Padding(2, 0, 2, 0);
-            label6.Name = "label6";
-            label6.Size = new Size(33, 15);
-            label6.TabIndex = 4;
-            label6.Text = "Price";
+            inpt_genre.Location = new Point(181, 167);
+            inpt_genre.Margin = new Padding(2, 1, 2, 1);
+            inpt_genre.Name = "inpt_genre";
+            inpt_genre.Size = new Size(234, 23);
+            inpt_genre.TabIndex = 16;
             // 
-            // label5
+            // lbl_genre
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(46, 168);
-            label5.Margin = new Padding(2, 0, 2, 0);
-            label5.Name = "label5";
-            label5.Size = new Size(82, 15);
-            label5.TabIndex = 3;
-            label5.Text = "Purchase Date";
+            lbl_genre.AutoSize = true;
+            lbl_genre.Location = new Point(45, 167);
+            lbl_genre.Margin = new Padding(2, 0, 2, 0);
+            lbl_genre.Name = "lbl_genre";
+            lbl_genre.Size = new Size(38, 15);
+            lbl_genre.TabIndex = 15;
+            lbl_genre.Text = "Genre";
             // 
-            // label4
+            // btn_cancel
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(46, 124);
-            label4.Margin = new Padding(2, 0, 2, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(67, 15);
-            label4.TabIndex = 2;
-            label4.Text = "Publication";
+            btn_cancel.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_cancel.Location = new Point(325, 345);
+            btn_cancel.Margin = new Padding(2, 1, 2, 1);
+            btn_cancel.Name = "btn_cancel";
+            btn_cancel.Size = new Size(90, 26);
+            btn_cancel.TabIndex = 14;
+            btn_cancel.Text = "Cancel";
+            btn_cancel.UseVisualStyleBackColor = true;
+            btn_cancel.Click += btn_cancel_Click;
             // 
-            // label3
+            // btn_save
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(46, 75);
-            label3.Margin = new Padding(2, 0, 2, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(44, 15);
-            label3.TabIndex = 1;
-            label3.Text = "Author";
+            btn_save.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_save.Location = new Point(182, 345);
+            btn_save.Margin = new Padding(2, 1, 2, 1);
+            btn_save.Name = "btn_save";
+            btn_save.Size = new Size(101, 26);
+            btn_save.TabIndex = 13;
+            btn_save.Text = "Save";
+            btn_save.UseVisualStyleBackColor = true;
+            btn_save.Click += btn_save_Click;
             // 
-            // label2
+            // inpt_quantity
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(46, 28);
-            label2.Margin = new Padding(2, 0, 2, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(69, 15);
-            label2.TabIndex = 0;
-            label2.Text = "Book Name";
+            inpt_quantity.Location = new Point(182, 300);
+            inpt_quantity.Margin = new Padding(2, 1, 2, 1);
+            inpt_quantity.Name = "inpt_quantity";
+            inpt_quantity.Size = new Size(234, 23);
+            inpt_quantity.TabIndex = 11;
+            // 
+            // inpt_isbn
+            // 
+            inpt_isbn.Location = new Point(182, 255);
+            inpt_isbn.Margin = new Padding(2, 1, 2, 1);
+            inpt_isbn.Name = "inpt_isbn";
+            inpt_isbn.Size = new Size(234, 23);
+            inpt_isbn.TabIndex = 10;
+            // 
+            // inpt_publisher
+            // 
+            inpt_publisher.Location = new Point(182, 121);
+            inpt_publisher.Margin = new Padding(2, 1, 2, 1);
+            inpt_publisher.Name = "inpt_publisher";
+            inpt_publisher.Size = new Size(234, 23);
+            inpt_publisher.TabIndex = 8;
+            // 
+            // inpt_author
+            // 
+            inpt_author.Location = new Point(182, 72);
+            inpt_author.Margin = new Padding(2, 1, 2, 1);
+            inpt_author.Name = "inpt_author";
+            inpt_author.Size = new Size(234, 23);
+            inpt_author.TabIndex = 7;
+            // 
+            // inpt_bookTitle
+            // 
+            inpt_bookTitle.Location = new Point(182, 26);
+            inpt_bookTitle.Margin = new Padding(2, 1, 2, 1);
+            inpt_bookTitle.Name = "inpt_bookTitle";
+            inpt_bookTitle.Size = new Size(234, 23);
+            inpt_bookTitle.TabIndex = 6;
+            // 
+            // lbl_quantity
+            // 
+            lbl_quantity.AutoSize = true;
+            lbl_quantity.Location = new Point(46, 302);
+            lbl_quantity.Margin = new Padding(2, 0, 2, 0);
+            lbl_quantity.Name = "lbl_quantity";
+            lbl_quantity.Size = new Size(53, 15);
+            lbl_quantity.TabIndex = 5;
+            lbl_quantity.Text = "Quantity";
+            // 
+            // lbl_isbn
+            // 
+            lbl_isbn.AutoSize = true;
+            lbl_isbn.Location = new Point(46, 255);
+            lbl_isbn.Margin = new Padding(2, 0, 2, 0);
+            lbl_isbn.Name = "lbl_isbn";
+            lbl_isbn.Size = new Size(32, 15);
+            lbl_isbn.TabIndex = 4;
+            lbl_isbn.Text = "ISBN";
+            // 
+            // lbl_publisher
+            // 
+            lbl_publisher.AutoSize = true;
+            lbl_publisher.Location = new Point(46, 124);
+            lbl_publisher.Margin = new Padding(2, 0, 2, 0);
+            lbl_publisher.Name = "lbl_publisher";
+            lbl_publisher.Size = new Size(56, 15);
+            lbl_publisher.TabIndex = 2;
+            lbl_publisher.Text = "Publisher";
+            // 
+            // lbl_author
+            // 
+            lbl_author.AutoSize = true;
+            lbl_author.Location = new Point(46, 75);
+            lbl_author.Margin = new Padding(2, 0, 2, 0);
+            lbl_author.Name = "lbl_author";
+            lbl_author.Size = new Size(44, 15);
+            lbl_author.TabIndex = 1;
+            lbl_author.Text = "Author";
+            // 
+            // lbl_bookTitle
+            // 
+            lbl_bookTitle.AutoSize = true;
+            lbl_bookTitle.Location = new Point(46, 28);
+            lbl_bookTitle.Margin = new Padding(2, 0, 2, 0);
+            lbl_bookTitle.Name = "lbl_bookTitle";
+            lbl_bookTitle.Size = new Size(59, 15);
+            lbl_bookTitle.TabIndex = 0;
+            lbl_bookTitle.Text = "Book Title";
             // 
             // AddBooks
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(546, 421);
+            ClientSize = new Size(546, 511);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -249,19 +273,21 @@
         private Label label1;
         private PictureBox pictureBox1;
         private Panel panel2;
+        private TextBox inpt_bookTitle;
+        private Label lbl_quantity;
+        private Label lbl_publisher;
+        private Label lbl_author;
+        private Label lbl_bookTitle;
+        private Button btn_cancel;
+        private Button btn_save;
+        private TextBox inpt_quantity;
+        private TextBox inpt_publisher;
+        private TextBox inpt_author;
+        private TextBox inpt_isbn;
+        private Label lbl_isbn;
+        private TextBox inpt_genre;
+        private Label lbl_genre;
         private TextBox textBox1;
-        private Label label7;
-        private Label label6;
-        private Label label5;
-        private Label label4;
-        private Label label3;
-        private Label label2;
-        private Button button2;
-        private Button button1;
-        private DateTimePicker dateTimePicker1;
-        private TextBox textBox6;
-        private TextBox textBox5;
-        private TextBox textBox3;
-        private TextBox textBox2;
+        private Label lbl_language;
     }
 }
