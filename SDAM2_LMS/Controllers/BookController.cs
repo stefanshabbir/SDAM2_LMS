587fe77711bab2f08743d1b7dd93394c59b37573
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2025-01-20 19:32:44 +0530

    feat: BookController initial

---
+++ SDAM2_LMS/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using SDAM2_LMS.Models.Data;
+using SDAM2_LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAM2_LMS.Controllers
+{
+    internal class BookController
+    {
+        private readonly DatabaseContext _context;
+        public BookController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook()
+        {
+
+        }
+    }
+}
