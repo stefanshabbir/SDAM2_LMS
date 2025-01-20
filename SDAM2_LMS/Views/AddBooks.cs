225267377151844db6ef59c39c3fb61c7a7bac33
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2025-01-20 20:00:26 +0530

    feat: AddBook variable declarations

--- SDAM2_LMS/Views/AddBooks.cs
+++ SDAM2_LMS/Views/AddBooks.cs
@@ -22,7 +22,15 @@ namespace SDAM2_LMS
 
         private void btn_save_Click(object sender, EventArgs e)
         {
-
+            // NO ERROR CHECKING--
+            string title = inpt_bookTitle.Text;
+            string author = inpt_author.Text;
+            string genre = inpt_genre.Text;
+            string publisher = inpt_publisher.Text;
+            string language = inpt_language.Text;
+            string isbn = inpt_isbn.Text;
+            int quantity = int.TryParse(inpt_quantity);
+            // --
         }
 
         private void btn_cancel_Click(object sender, EventArgs e)
