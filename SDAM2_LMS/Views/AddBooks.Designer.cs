69c611d6b5ea86d8f7fc3118e1c7cc32d409ca6b
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2025-01-20 15:50:55 +0530

    feat: Login Page and Dashboard connected

--- SDAM2_LMS/Views/AddBooks.Designer.cs
+++ SDAM2_LMS/Views/AddBooks.Designer.cs
@@ -54,9 +54,10 @@
             panel1.BackColor = Color.Gray;
             panel1.Controls.Add(label1);
             panel1.ForeColor = SystemColors.ActiveCaption;
-            panel1.Location = new Point(0, 2);
+            panel1.Location = new Point(0, 1);
+            panel1.Margin = new Padding(2, 1, 2, 1);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1133, 144);
+            panel1.Size = new Size(546, 68);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -64,9 +65,10 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 19.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Location = new Point(563, 35);
+            label1.Location = new Point(188, 8);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(351, 71);
+            label1.Size = new Size(180, 37);
             label1.TabIndex = 0;
             label1.Text = "ADD BOOKS";
             // 
@@ -87,17 +89,19 @@
             panel2.Controls.Add(label4);
             panel2.Controls.Add(label3);
             panel2.Controls.Add(label2);
-            panel2.Location = new Point(303, 147);
+            panel2.Location = new Point(47, 71);
+            panel2.Margin = new Padding(2, 1, 2, 1);
             panel2.Name = "panel2";
-            panel2.Size = new Size(830, 724);
+            panel2.Size = new Size(447, 339);
             panel2.TabIndex = 2;
             // 
             // button2
             // 
             button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(603, 646);
+            button2.Location = new Point(325, 303);
+            button2.Margin = new Padding(2, 1, 2, 1);
             button2.Name = "button2";
-            button2.Size = new Size(167, 56);
+            button2.Size = new Size(90, 26);
             button2.TabIndex = 14;
             button2.Text = "Cancel";
             button2.UseVisualStyleBackColor = true;
@@ -105,117 +109,131 @@
             // button1
             // 
             button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(338, 646);
+            button1.Location = new Point(182, 303);
+            button1.Margin = new Padding(2, 1, 2, 1);
             button1.Name = "button1";
-            button1.Size = new Size(187, 56);
+            button1.Size = new Size(101, 26);
             button1.TabIndex = 13;
             button1.Text = "Save";
             button1.UseVisualStyleBackColor = true;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(338, 352);
+            dateTimePicker1.Location = new Point(182, 165);
+            dateTimePicker1.Margin = new Padding(2, 1, 2, 1);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(432, 39);
+            dateTimePicker1.Size = new Size(234, 23);
             dateTimePicker1.TabIndex = 12;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(338, 555);
+            textBox6.Location = new Point(182, 260);
+            textBox6.Margin = new Padding(2, 1, 2, 1);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(432, 39);
+            textBox6.Size = new Size(234, 23);
             textBox6.TabIndex = 11;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(338, 458);
+            textBox5.Location = new Point(182, 215);
+            textBox5.Margin = new Padding(2, 1, 2, 1);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(432, 39);
+            textBox5.Size = new Size(234, 23);
             textBox5.TabIndex = 10;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(338, 258);
+            textBox3.Location = new Point(182, 121);
+            textBox3.Margin = new Padding(2, 1, 2, 1);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(432, 39);
+            textBox3.Size = new Size(234, 23);
             textBox3.TabIndex = 8;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(338, 153);
+            textBox2.Location = new Point(182, 72);
+            textBox2.Margin = new Padding(2, 1, 2, 1);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(432, 39);
+            textBox2.Size = new Size(234, 23);
             textBox2.TabIndex = 7;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(338, 56);
+            textBox1.Location = new Point(182, 26);
+            textBox1.Margin = new Padding(2, 1, 2, 1);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(432, 39);
+            textBox1.Size = new Size(234, 23);
             textBox1.TabIndex = 6;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(85, 558);
+            label7.Location = new Point(46, 262);
+            label7.Margin = new Padding(2, 0, 2, 0);
             label7.Name = "label7";
-            label7.Size = new Size(106, 32);
+            label7.Size = new Size(53, 15);
             label7.TabIndex = 5;
             label7.Text = "Quantity";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(85, 459);
+            label6.Location = new Point(46, 215);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(65, 32);
+            label6.Size = new Size(33, 15);
             label6.TabIndex = 4;
             label6.Text = "Price";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(85, 359);
+            label5.Location = new Point(46, 168);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(166, 32);
+            label5.Size = new Size(82, 15);
             label5.TabIndex = 3;
             label5.Text = "Purchase Date";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(85, 265);
+            label4.Location = new Point(46, 124);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(132, 32);
+            label4.Size = new Size(67, 15);
             label4.TabIndex = 2;
             label4.Text = "Publication";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(85, 160);
+            label3.Location = new Point(46, 75);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(87, 32);
+            label3.Size = new Size(44, 15);
             label3.TabIndex = 1;
             label3.Text = "Author";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(85, 59);
+            label2.Location = new Point(46, 28);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(139, 32);
+            label2.Size = new Size(69, 15);
             label2.TabIndex = 0;
             label2.Text = "Book Name";
             // 
             // AddBooks
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1134, 871);
+            ClientSize = new Size(546, 421);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(2, 1, 2, 1);
             Name = "AddBooks";
             Text = "AddBooks";
             panel1.ResumeLayout(false);
