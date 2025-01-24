73b7ba9b03b2e6daab189c9a36745cac6dd147fb
Yumna-H <yumnahassan06@gmail.com>
2025-01-25 00:25:31 +0530

    feat: Improve error handling for login process

--- SDAM2_LMS/Views/LoginPage.cs
+++ SDAM2_LMS/Views/LoginPage.cs
@@ -44,36 +44,47 @@ namespace SDAM2_LMS
                 return;
             }
 
-            var controller = new AccountController(new AccountService(new DatabaseContext()));
-            var user = controller.Login(username, password);
-
-            if (user != null)
+            try
             {
-                if (user.AccountTypeID == ADMIN)
+                var controller = new AccountController(new AccountService(new DatabaseContext()));
+                var user = controller.Login(username, password);
+
+                if (user != null)
                 {
-                    AdminDashboard dashboard = new AdminDashboard();
-                    dashboard.Show();
-                    this.Hide();
-                }
-                else if (user.AccountTypeID == LIBRARIAN)
-                {
-                    LibrarianDashboard dashboard = new LibrarianDashboard();
-                    dashboard.Show();
-                    this.Hide();
+                    if (user.AccountTypeID == ADMIN)
+                    {
+                        AdminDashboard dashboard = new AdminDashboard();
+                        dashboard.Show();
+                        this.Hide();
+                    }
+                    else if (user.AccountTypeID == LIBRARIAN)
+                    {
+                        LibrarianDashboard dashboard = new LibrarianDashboard();
+                        dashboard.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        //TODO: Log into member's dashboard
+                    }
+
                 }
                 else
                 {
-                    //TODO: Log into member's dashboard
+                    MessageBox.Show("Invalid Username or Password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    textBoxUsername.Clear();
+                    textBoxPassword.Clear();
+                    textBoxUsername.Focus();
                 }
-
             }
-            else
+            catch (ArgumentException ex)
             {
-                MessageBox.Show("Invalid Username or Password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                textBoxUsername.Clear();
-                textBoxPassword.Clear();
-                textBoxUsername.Focus();
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unexpected error occurred. Please try again later.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
         }
